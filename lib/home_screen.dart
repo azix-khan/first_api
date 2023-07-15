e2fdd84b2608f34821f483e4248da53d69d2edb8
azix-khan <azixkhan.55@gmail.com>
2023-07-16 00:56:31 +0500

    first api

---
+++ lib/home_screen.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:api_demo/Models/PostsModel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      postList.clear();
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Api Course'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                  future: getPostApi(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Text('Loding');
+                    } else {
+                      return ListView.builder(
+                          itemCount: postList.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Title',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.pink,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      postList[index].title.toString(),
+                                    ),
+                                    const Text(
+                                      'Description',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.pinkAccent,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(postList[index].body.toString()),
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    }
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
