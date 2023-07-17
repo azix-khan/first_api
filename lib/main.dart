2a519d4decdbb8f4ae8750fbe41828a83534d1f9
azix-khan <azixkhan.55@gmail.com>
2023-07-18 00:21:25 +0500

    complex api without json model

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:api_demo/example_four.dart';
 import 'package:api_demo/example_three.dart';
 // import 'package:api_demo/example_two.dart';
 // import 'package:api_demo/home_screen.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ExampleThree(),
+      home: const ExampleFour(),
     );
   }
 }
